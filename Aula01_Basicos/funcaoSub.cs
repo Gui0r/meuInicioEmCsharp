11b35f65d88defcf0fa8857b13f54b6e8cc8dcfb
Guilherme Michel <139629718+Gui0r@users.noreply.github.com>
2024-04-18 15:28:22 -0300

    organizando os arquivos e fazendo a 2 aula

---
+++ Aula01_Basicos/funcaoSub.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+public static int subtrair(int a, int b)
+{
+    int subtrair = a - b;
+    return subtrair;
+}
+static void Main(string[]args)
+{
+    int resultado = subtrair(5, 8);
+    Console.WriteLine("O resultado da subtração foi: " + resultado);
+    Console.ReadKey
+}
+}
